24c465525791258abe0a11be3f8a0d8e997a3f9f
Rishab Bhatt <bhattrishu07@gmail.com>
2024-10-18 12:17:29 +0530

    hero animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,10 @@ const Hero = () => {
         className="h-full p-5 flex flex-col lg:flex-row justify-around border-b border-gray-300 "
         id="home"
       >
-        <div className=" w-[30%] sm:mx-auto mr-[60%] p-3 sm:ml-20">
+        <div
+          className=" w-[30%] sm:mx-auto mr-[60%] p-3 sm:ml-20"
+          data-aos="fade-up"
+        >
           <h1 className="hero-text font-inter font-extrabold mx-auto text-4xl lg:mt-20 lg:text-6xl p-5">
             MY NAME IS RISHAB BHATT,
             <span className="text-red-400">FRONTEND ENGINEER</span>
@@ -18,6 +21,7 @@ const Hero = () => {
         <img
           src={hero}
           className="h-[450px] w-[400px] mx-auto sm:mx-auto sm:h-[450px] sm:w-[450px] lg:h-auto lg:w-auto lg:mb-10"
+          data-aos="zoom-in"
         />
       </div>
     </>
